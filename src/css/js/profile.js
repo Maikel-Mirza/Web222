d3d87e7d064a7bc25f7e73a57c193a65a37e22b4
Maikel Mirzadegan <91105959+Maikel-Mirza@users.noreply.github.com>
2022-08-13 01:43:26 +0000

    Add files via upload

---
+++ src/css/js/profile.js
@@ -0,0 +1,37 @@
+/**
+ * WEB222 – FINAL ASSESSMENT
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Maikel Mirzadegan
+ *      Student ID: 032299158
+ *      Date:       August 14,2022
+ */
+ window.profile = {
+    name: "Maikel Mirzadegan",
+    title: "Student",
+    aboutMe:
+      "Im a computer programming student at Seneca College and currently Im completing my second semester of my program. I have been programming for two years, and this consists of programming in languages like c, c++, sql, and js. Im passionate about this subject and enjoy the projects and assignments that I have to complete and learn from. I love learning new things and further developing my understanding in these languages and in this field. I enjoy working in team environments and multicutural environments.",
+    email: "mmirzadegan@myseneca.ca",
+    courseCode: "WEB222",
+    section: "NHH",
+    studentId: "032299158",
+    imageUrl: "https://garciarg.github.io/web222-final/images/mypic.jpg",
+    language: "English",
+    instructor: "Professor Siyavash Ghassemi Nia",
+    education: [
+      {
+        institution: "Seneca College",
+        degree: "College Diploma",
+        program: "Computer Programming",
+        period: "2022 - 2023 (expected)",
+        address: "Newnham Campus, Finch Avenue East, Toronto, Ontario, Canada",
+      },
+    ],
+  };
+  
